404cd1cae6bb32cc9489ba4bd5b9386c481b7ca4
lamogui <julien.deloor@gmail.com>
2014-11-08 18:03:16 +0100

    Convertion du lm75 pour être utilisé avec i2c bus

--- include/lm75.hpp
+++ include/lm75.hpp
@@ -2,18 +2,20 @@
 #ifndef I2C_LM75
 #define I2C_LM75
 
-#include "i2c_interface.hpp"
+#include "i2c_slave.hpp"
 
 #define TEMPERATURE_ADDR 0x0
 #define CONFIGURATION_ADDR 0x1
 #define THYST_ADDR 0x2
 #define TOS_ADDR 0x3
 
-class I2c_LM75 : public I2c_interface {
+#ifndef SIMULATION
+
+class LM75 : public i2c_slave {
 
   public:
-    I2c_LM75(int bus, uint8_t slave_addr);
-    virtual ~I2c_LM75();
+    LM75(i2c_bus& bus, uint8_t addr);
+    virtual ~LM75();
 
     float temp_hextofloat(uint8_t * temp_hex);
     void temp_floattohex(uint8_t * temp_hex, float temp_float);
@@ -22,5 +24,6 @@ class I2c_LM75 : public I2c_interface {
   private:
 };
 
-#endif
+#endif //SIMULATION
+#endif //HEADER
 
--- src/lm75.cpp
+++ src/lm75.cpp
@@ -1,32 +1,35 @@
 
-#include "i2c_LM75.hpp"
+#include "lm75.hpp"
 
-I2c_LM75::I2c_LM75(int bus, uint8_t slave_addr) :
-I2c_interface(bus, slave_addr)
-{}
+#ifndef SIMULATION
+
+LM75::LM75(i2c_bus& bus, uint8_t slave_addr) :
+i2c_slave(bus, slave_addr)
+{
+}
 
 
-I2c_LM75::~I2c_LM75() {
+LM75::~LM75() {
 
 }
 
-float I2c_LM75::get_temp() {
+float LM75::get_temp() {
   uint8_t temp[2]={0};
-  int r=i2c_read(0x0, temp, 2);
+  int r=fast_read(0x0, temp, 2);
   if (r==-1) {
     return 301.0; //not in temperature range
   }
   return temp_hextofloat(temp);
 }
 
-float I2c_LM75::temp_hextofloat(uint8_t * temp_hex) {
+float LM75::temp_hextofloat(uint8_t * temp_hex) {
   int signe=((temp_hex[0]>>7)?-1:1);
   int partie_entiere=((temp_hex[0])&(~(1<<7)));
   float partie_decimale=(((temp_hex[1]>>7)?0.5:0));
   float temp_float=signe * (partie_entiere + partie_decimale);
   return temp_float;
 }
-void I2c_LM75::temp_floattohex(uint8_t * temp_hex, float temp_float) {
+void LM75::temp_floattohex(uint8_t * temp_hex, float temp_float) {
   int temp = (int)(temp_float*10);
   temp_hex[0]=(abs(temp/10)&(0xFF>>1));
   if (temp_float<0)
@@ -35,3 +38,5 @@ void I2c_LM75::temp_floattohex(uint8_t * temp_hex, float temp_float) {
     temp_hex[0]&=~(1<<7);
   temp_hex[1]=(uint8_t)((temp%10)>=5)?0x80:0;
 }
+
+#endif
