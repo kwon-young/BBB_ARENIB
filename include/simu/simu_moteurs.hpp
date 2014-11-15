8e07811f60a575cd386f9ff4e64c0dfdf0e24c4e
lamogui <julien.deloor@gmail.com>
2014-11-15 16:16:44 +0100

    Ajout prototype de classe simu_moteur

---
+++ include/simu/simu_moteurs.hpp
@@ -0,0 +1,31 @@
+
+
+#ifndef SIMU_MOTEURS____
+#define SIMU_MOTEURS____
+
+class Simu_moteurs
+{
+  public:
+    Simu_moteurs(float voltage=21.0);
+    
+    float pwmR; //-1.0 to 1.0;
+    float pwmL; 
+    
+    void update(float dt);
+    
+    inline float getSpeedR() { // rad/sec
+      return _speedR;
+    }
+    
+    inline float getSpeedL() {
+      return _speedL;
+    }
+  
+  private:
+    float _speedL;
+    float _speedR;
+    float _alim;
+    
+};
+
+#endif
